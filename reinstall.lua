04612a9f97ec074d48d91f4933835e18cc5c113d
themagicpiston <themagicpistonbusiness@gmail.com>
2026-07-20 18:34:48 -0400

    Create reinstall.lua

---
+++ reinstall.lua
@@ -0,0 +1,4 @@
+delfolder(‘pistonware’)
+task.wait(1)
+shared.VapeSmoothBoot = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/themagicpiston/pistonware/refs/heads/main/loader.lua", true))()
